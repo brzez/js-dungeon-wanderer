281eb47974b481a641922f84ed9817c1b21fa595
brzez <pawel@brzez.com>
2016-09-22 13:13:41 +0200

    encoding form data

--- src/client.js
+++ src/client.js
@@ -1,6 +1,8 @@
 import request from './client/request'
 import layers from './client/layers'
 
+import {serializeForm} from './client/form'
+
 
 // request('get', '/', function(state, xhr) {
 //     layers(state, function() {
@@ -8,30 +10,6 @@ import layers from './client/layers'
 //     })
 // });
 
-let serializeForm = function(form) {
-    // first - group the inputs by name
-    let grouped = {};
-    for(let index = 0; index < form.elements.length; index++){
-        let element = form.elements[index];
-        if(element.disabled || !element.name) continue;
-        if(!grouped[element.name]){
-            grouped[element.name] = [];
-        }
-        grouped[element.name].push(element);
-    }
-    let data = {};
-    // build data object [{name=>value}]
-    for(var name in grouped){
-        var elements = [].slice.call(grouped[name]);
-        // dataset.submitted elements have priority
-        data[name] = elements.sort(function(a, b) {
-            return b.dataset.submitted - a.dataset.submitted;
-        }).map(function(o) {
-            return o.value;
-        }).shift();
-    }
-    return data;
-}
 
 // hijack all forms
 document.querySelectorAll('form').forEach(function(form) {
@@ -48,7 +26,10 @@ document.querySelectorAll('form').forEach(function(form) {
     form.onsubmit = function(e) {
         console.log('form#onsubmit')
         e.preventDefault();
-        let data = serializeForm(this);
-        console.log(data)
+        request('post', '/', function(gameState) {
+            layers(gameState, function() {
+                console.log.apply(console, arguments)
+            })
+        }, {data: serializeForm(this)})
     }
 })
--- src/client/request.js
+++ src/client/request.js
@@ -1,15 +1,17 @@
 
 
-var request = function(method, url, callback, responseType) {
+var request = function(method, url, callback, options) {
+    options = options || {};
+
     var xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
     xhr.setRequestHeader('x-requested-with', 'xmlhttprequest');
-    xhr.responseType = responseType || 'json';
+    xhr.responseType = options.responseType || 'json';
     xhr.onreadystatechange = function() {
         if(xhr.readyState !== XMLHttpRequest.DONE) return;
         callback(xhr.response, xhr);
     }
-    xhr.send();
+    xhr.send(options.data);
 }
 
 export default request;
