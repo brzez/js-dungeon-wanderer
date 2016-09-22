ecda1a330f4616cbe9ed498a06f2f319638d2a0c
brzez <pawel@brzez.com>
2016-09-22 13:18:16 +0200

    added content-type

--- src/client/request.js
+++ src/client/request.js
@@ -4,8 +4,14 @@ var request = function(method, url, callback, options) {
     options = options || {};
 
     var xhr = new XMLHttpRequest();
+
     xhr.open(method, url, true);
     xhr.setRequestHeader('x-requested-with', 'xmlhttprequest');
+
+    if(method.match(/post/i)){
+        xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded');
+    }
+
     xhr.responseType = options.responseType || 'json';
     xhr.onreadystatechange = function() {
         if(xhr.readyState !== XMLHttpRequest.DONE) return;
