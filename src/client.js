ca7c9e70feb63501ae69bbb6ae492ef2b7314016
brzez <pawel@brzez.com>
2016-09-26 13:25:22 +0200

    casting querySelectorAll to array (ffx fix)

--- src/client.js
+++ src/client.js
@@ -3,12 +3,16 @@ import renderLayers from './client/layers'
 
 import {serializeForm} from './client/form'
 
+var toArray = function(o) {
+    return [].slice.call(o)
+}
+
 /**
  * Forwards form actions as xhr post
  */
 var bind = function(){
-    document.querySelectorAll('form').forEach(function(form) {
-        var buttons = form.querySelectorAll('button');
+    toArray(document.querySelectorAll('form')).forEach(function(form) {
+        var buttons = toArray(form.querySelectorAll('button'));
         buttons.forEach(function(button) {
             button.onclick = function() {
                 buttons.forEach(function(btn) {
